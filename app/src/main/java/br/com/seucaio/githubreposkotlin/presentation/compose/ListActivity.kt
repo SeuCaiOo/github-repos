be68e903ae4d23e3a001f9acd7f8465a0090df62
SeuCaiOo <caiopimentel8@gmail.com>
2022-03-13 11:24:25 -0300

    componetizing Activity

--- app/src/main/java/br/com/seucaio/githubreposkotlin/presentation/compose/ListActivity.kt
+++ app/src/main/java/br/com/seucaio/githubreposkotlin/presentation/compose/ListActivity.kt
@@ -3,36 +3,50 @@ package br.com.seucaio.githubreposkotlin.presentation.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.seucaio.githubreposkotlin.presentation.compose.ui.theme.GithubreposkotlinTheme
 
 class ListActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GithubreposkotlinTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp {
+                Greeting("Android")
             }
+        }
+    }
+
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    GithubreposkotlinTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+            content()
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier.padding(16.dp)
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    GithubreposkotlinTheme {
+    MyApp {
         Greeting("Android")
     }
 }
