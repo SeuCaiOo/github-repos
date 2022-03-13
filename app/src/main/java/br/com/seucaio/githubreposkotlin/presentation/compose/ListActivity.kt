b0f32b278a7f7586f48396d807c046cefa6cff54
SeuCaiOo <caiopimentel8@gmail.com>
2022-03-13 13:27:22 -0300

    added animation

--- app/src/main/java/br/com/seucaio/githubreposkotlin/presentation/compose/ListActivity.kt
+++ app/src/main/java/br/com/seucaio/githubreposkotlin/presentation/compose/ListActivity.kt
@@ -3,7 +3,9 @@ package br.com.seucaio.githubreposkotlin.presentation.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
@@ -12,6 +14,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.seucaio.githubreposkotlin.presentation.compose.ui.theme.GithubreposkotlinTheme
@@ -72,10 +75,19 @@ fun Counter(count: Int, updateCount: ((Int) -> Unit)) {
 
 @Composable
 fun Greeting(name: String) {
-    Text(
-        text = "Hello $name!",
-        modifier = Modifier.padding(16.dp)
+    var isSelected by remember { mutableStateOf(false) }
+    val targetColor by animateColorAsState(
+        targetValue = if (isSelected) MaterialTheme.colors.primary else Color.Transparent,
+        animationSpec = tween(durationMillis = 1000)
     )
+    Surface(color = targetColor) {
+        Text(
+            text = "Hello $name!",
+            modifier = Modifier
+                .clickable { isSelected = !isSelected }
+                .padding(16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
