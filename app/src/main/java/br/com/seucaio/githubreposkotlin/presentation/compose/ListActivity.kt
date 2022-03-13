98a3fbe0b367d3389b113d55c55afa3d1888ccd9
SeuCaiOo <caiopimentel8@gmail.com>
2022-03-13 12:35:49 -0300

    changed Colum to recyclerView

--- app/src/main/java/br/com/seucaio/githubreposkotlin/presentation/compose/ListActivity.kt
+++ app/src/main/java/br/com/seucaio/githubreposkotlin/presentation/compose/ListActivity.kt
@@ -3,8 +3,12 @@ package br.com.seucaio.githubreposkotlin.presentation.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -35,25 +39,32 @@ fun MyApp(content: @Composable () -> Unit) {
 }
 
 @Composable
-fun MyScreenContent(names: List<String> = listOf("Android", "iOS", "Windows")) {
+fun MyScreenContent(names: List<String> = List(100) { "Android $it" }) {
     var counterState by remember { mutableStateOf<Int>(0) }
-    Column {
-        for (name in names) {
-            Greeting(name = name)
-            Divider()
-        }
+    Column(modifier = Modifier.fillMaxHeight()) {
+        NameList(names = names, modifier = Modifier.Companion.weight(1f))
         Counter(
             count = counterState,
             updateCount = { newCount: Int -> counterState = newCount }
         )
-        if(counterState > 5) {
+        if (counterState > 5) {
             Text(text = "Exagerou na contagem")
         }
-     }
+    }
 }
 
 @Composable
-fun Counter(count: Int, updateCount: ((Int) -> Unit) ) {
+fun NameList(names: List<String>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(items = names) { name: String ->
+            Greeting(name = name)
+            Divider()
+        }
+    }
+}
+
+@Composable
+fun Counter(count: Int, updateCount: ((Int) -> Unit)) {
     Button(onClick = { updateCount(count + 1) }) {
         Text(text = "Voce clicou $count vezes")
     }
