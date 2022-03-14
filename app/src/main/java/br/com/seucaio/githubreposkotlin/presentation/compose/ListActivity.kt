548ca6a1247d88fa7856223e904a2d41a40b420d
SeuCaiOo <caiopimentel8@gmail.com>
2022-03-14 12:26:05 -0300

    created layout list

--- app/src/main/java/br/com/seucaio/githubreposkotlin/presentation/compose/ListActivity.kt
+++ app/src/main/java/br/com/seucaio/githubreposkotlin/presentation/compose/ListActivity.kt
@@ -6,21 +6,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.seucaio.githubreposkotlin.R
+import br.com.seucaio.githubreposkotlin.domain.entity.Owner
 import br.com.seucaio.githubreposkotlin.domain.entity.Repo
 import br.com.seucaio.githubreposkotlin.presentation.compose.ui.theme.GithubreposkotlinTheme
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
 
 class ListActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,16 +52,23 @@ fun MyApp(content: @Composable () -> Unit) {
 
 @Composable
 fun MyScreenContent(names: List<String> = List(100) { "Android $it" }) {
-    var counterState by rememberSaveable { mutableStateOf<Int>(0) }
+    val repo = Repo(
+        id = 5152285,
+        name = "okhttp",
+        fullName = "square/okhttp",
+        description = "Square’s meticulous HTTP client for the JVM, Android, and GraalVM.",
+        language = "Kotlin",
+        forksCount = 8792,
+        stargazersCount = 41700,
+        owner = Owner(
+            id = 82592,
+            login = "square",
+            avatarUrl = "https://avatars.githubusercontent.com/u/82592?v=4",
+        ),
+    )
+
     Column(modifier = Modifier.fillMaxHeight()) {
-        NameList(names = names, modifier = Modifier.Companion.weight(1f))
-        Counter(
-            count = counterState,
-            updateCount = { newCount: Int -> counterState = newCount }
-        )
-        if (counterState > 5) {
-            Text(text = "Exagerou na contagem")
-        }
+        RepoList(list = listOf(repo), modifier = Modifier.Companion.weight(1f))
     }
 }
 
@@ -92,6 +105,50 @@ fun Greeting(name: String) {
         )
     }
 }
+
+@Composable
+fun RepoList(list: List<Repo>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(items = list) { repo: Repo ->
+            RepoContent(repo = repo)
+            Divider()
+        }
+    }
+}
+
+
+@OptIn(ExperimentalCoilApi::class)
+@Composable
+fun RepoContent(repo: Repo) {
+    Row(modifier = Modifier.padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = rememberImagePainter(data = repo.owner?.avatarUrl),
+            contentDescription = "",
+            modifier = Modifier.size(40.dp),
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column(modifier = Modifier.weight(1f)) {
+            Text(
+                text = "${repo.name}",
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.body2,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Text(text = "${repo.fullName}",
+                style = MaterialTheme.typography.caption,
+                modifier = Modifier.fillMaxWidth(),
+            )
+            Text(
+                text = "${repo.owner?.login}",
+                fontSize = 20.sp,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
+
 
 @Preview(name = "Light Mode", showBackground = true)
 @Preview(
